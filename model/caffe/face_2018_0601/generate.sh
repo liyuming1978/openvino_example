81f1291e4b008b0d052f34b51ebcbfee28816d5e
liyuming <lym@lym5808.sh.intel.com>
2018-07-04 12:54:22 +0800

    add caffe model

---
+++ model/caffe/face_2018_0601/generate.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+source /opt/intel/computer_vision_sdk/bin/setupvars.sh 
+python3  /opt/intel/computer_vision_sdk/deployment_tools/model_optimizer/mo_caffe.py -m caffe/facedet.caffemodel --data_type FP16 -b 1 --mean_values [1,1,1] -s 127.5	
+
+
